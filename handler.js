48aca8b4ade3851d043fb63ef8236af4e85c2952
Marc Galang <marc.galang@wunder.io>
2019-03-24 15:30:37 +0200

    Add initial logic for getting pull requests

--- handler.js
+++ handler.js
@@ -1,12 +1,28 @@
 'use strict';
 
+const Octokit = require('@octokit/rest');
+
 module.exports.githubPrNotify = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+  const octokit = new Octokit({
+    auth: `token ${process.env.GITHUB_TOKEN}`
+  })
+
+  const github_repositories = process.env.GITHUB_REPOSITORIES || '';
+
+  const repos = github_repositories.split(',');
+
+  let message = [];
+
+  for (let repo of repos) {
+    const { data } = await octokit.request(`GET /repos/${repo}/pulls`);
+
+    if (data.length) {
+      data.forEach(item => {
+        message.push([item.title, item.url]);
+      });
+    }
+  }
+
+  return message.map((value) => value.join('\n')).join('\n');
 };
 
